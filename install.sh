a6efaa981c0b5c0bba15980b260247b2af54c504
Jackson Foster <jackson.foster@rover.com>
2025-08-27 17:06:42 -0700

    changes

--- install.sh
+++ install.sh
@@ -14,6 +14,15 @@ nvim --headless "$@" +qa
 # Git config (safe to overwrite)
 ln -sf $(pwd)/.gitconfig ~/.gitconfig
 
+# shared bashrc and zshrc config
+if ! grep -q "# DOTFILES CUSTOM SHARED SHELL CONFIG" ~/.bashrc; then
+    echo -e "\n# DOTFILES CUSTOM SHARED SHELL CONFIG \nsource $(pwd)/.sharedrc.append" >> $(pwd)/.bashrc.append
+fi
+
+if ! grep -q "# DOTFILES CUSTOM SHARED SHELL CONFIG" ~/.zshrc; then
+    echo -e "\n# DOTFILES CUSTOM SHARED SHELL CONFIG \nsource $(pwd)/.sharedrc.append" >> $(pwd)/.zshrc.append
+fi
+
 # Append custom zshrc without losing Codespaces defaults
 if ! grep -q "# DOTFILES CUSTOM ZSHRC" ~/.zshrc; then
     echo -e "\n# DOTFILES CUSTOM ZSHRC\nsource $(pwd)/.zshrc.append" >> ~/.zshrc
@@ -23,5 +32,6 @@ fi
 if ! grep -q "# DOTFILES CUSTOM BASHRC" ~/.bashrc; then
     echo -e "\n# DOTFILES CUSTOM BASHRC\nsource $(pwd)/.bashrc.append" >> ~/.bashrc
 fi
+
 
 echo "Dotfiles setup complete!"
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,37 +1,84 @@
 require("lazy").setup({
-    { "neovim/nvim-lspconfig" },
-    { "hrsh7th/nvim-cmp" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "L3MON4D3/LuaSnip" },
-    { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
-    { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
-    { "windwp/nvim-spectre" },
-    { "lewis6991/gitsigns.nvim" },
-    { "tpope/vim-fugitive" },
-	{
-	  "mfussenegger/nvim-dap",
-	  config = function()
-	    require("dap-setup")
-	  end,
-	},
-    { "rcarriga/nvim-dap-ui" },
-{
-  "nvim-neotest/nvim-nio",
-  lazy = true,  -- load only when required
-},
-{
-  "williamboman/mason.nvim",
-  build = ":MasonUpdate",
-  config = function()
-    require("mason").setup()
-  end,
-},
-{
-  "williamboman/mason-lspconfig.nvim",
-  config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = { "pyright", "ts_ls", "eslint", "efm" },
-    })
-  end,
-},
+  -- LSP and completion
+  { "neovim/nvim-lspconfig" },
+  { "hrsh7th/nvim-cmp" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "L3MON4D3/LuaSnip" },
+
+  -- File explorer
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" }
+  },
+
+  -- Telescope core
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+
+  -- Telescope FZF Native
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "make",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("telescope").setup{
+        defaults = {
+          sorting_strategy = "ascending",
+          layout_config = { prompt_position = "top" },
+          path_display = { "smart" }, -- options: "smart", "truncate", "absolute"
+        },
+        extensions = {
+          fzf = {
+            fuzzy = true,                    -- enables fuzzy matching
+            override_file_sorter = true,     -- smarter path-based ranking
+            override_generic_sorter = true,
+            case_mode = "smart_case",        -- case-insensitive unless capitals
+          }
+        }
+      }
+
+      require("telescope").load_extension("fzf")
+    end
+  },
+
+  -- Search/replace
+  { "windwp/nvim-spectre" },
+
+  -- Git integrations
+  { "lewis6991/gitsigns.nvim" },
+  { "tpope/vim-fugitive" },
+
+  -- Debugging
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      require("dap-setup")
+    end,
+  },
+  { "rcarriga/nvim-dap-ui" },
+
+  -- Nvim nio (required by dap-ui)
+  {
+    "nvim-neotest/nvim-nio",
+    lazy = true, -- load only when required
+  },
+
+  -- Mason (LSP/DAP/Linters)
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright", "ts_ls", "eslint", "efm" },
+      })
+    end,
+  },
 })
